16d21683792314a78a33ebdad5f13394ccc9e5b8
Carlos Eduardo Witte <cewitte@icloud.com>
2025-03-11 12:13:30 -0300

    Initial Commit

---
+++ SwiftDataProject/SwiftDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataProjectApp.swift
+//  SwiftDataProject
+//
+//  Created by Carlos Eduardo Witte on 11/03/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
